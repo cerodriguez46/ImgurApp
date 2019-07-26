442a19613932eb989457e09f7c0212346b6adc1a
Chris Rodiguez <christophererodriguez70@gmail.com>
2019-07-25 23:47:33 -0500

    UPDATE: MainFragment with MainViewmodel and fragment layout files created and fragment_main specified as home destination in nav_graph.xml
    fragment_main will be the launching point for the app

---
+++ app/src/main/java/com/cer/imgurapp/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.cer.imgurapp
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/cer/imgurapp/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.cer.imgurapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
